a5462721739ec15498727ddb0c392d2187bf71bb
AmitBiswas1402 <amit.142biswas@gmail.com>
2024-12-10 16:37:55 +0530

    hero sections making

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -17,6 +17,8 @@ import {
 import { useUser } from "@clerk/nextjs";
 // import PurchaseTicket from "./PurchaseTicket";
 import { useRouter } from "next/navigation";
+import { useStorageUrl } from "@/lib/utils";
+import Image from "next/image";
 
 function EventCard({ eventId }: { eventId: Id<"events"> }) {
   const { user } = useUser();
@@ -29,6 +31,67 @@ function EventCard({ eventId }: { eventId: Id<"events"> }) {
     userId: user?.id ?? "",
   });
 
-  return <div>EventCard</div>;
+  const queuePosition = useQuery(api.waitingList.getQueuePosition, {
+    eventId,
+    userId: user?.id ?? "",
+  });
+
+  const imageUrl = useStorageUrl(event?.imageStorageId);
+
+  if (!event || !availability) {
+    return null;
+  }
+
+  const isPastEvent = event.eventDate < Date.now();
+  const isEventOwner = user?.id === event?.userId;
+
+  return (
+    <div
+      onClick={() => router.push(`/event/${eventId}`)}
+      className={`bg-white rounded-xl shadow-sm hover:shadow-lg transition-all duration-300 border border-gray-100 cursor-pointer overflow-hidden relative ${
+        isPastEvent ? "opacity-75 hover:opacity-100" : ""
+      }`}
+    >
+      {/* Event Image */}
+      {imageUrl && (
+        <div>
+          <Image
+            src={imageUrl}
+            alt={event.name}
+            fill
+            className="object-coover"
+            priority
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-black/50to to-transparent " />
+        </div>
+      )}
+
+      {/* Event  Details */}
+      <div className={`p-6 ${imageUrl ? "relative" : ""}`}>
+        <div className="flex justify-between items-start">
+          <div>
+            <div className="flex flex-col items-start gap-2">
+              {isEventOwner && (
+                <span className="inline-flex items-center gap-1 bg-blue-600/90 text-white px-2 py-1 rounded-full text-xs font-medium">
+                  <StarIcon className="w-3 h-3" />
+                  Your Event
+                </span>
+              )}
+              <h2 className="text-2xl font-bold text-gray-900">{event.name}</h2>
+            </div>
+            {isPastEvent && (
+              <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 mt-2">
+                Past Event
+              </span>
+            )}
+          </div>
+
+          {/* Price Tag */}
+          
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default EventCard;
